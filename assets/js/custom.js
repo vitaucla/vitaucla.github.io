a5c27045407624e872ea4a8d9c9b76ee222000a4
Ardis Lu <ArdisGLu@gmail.com>
2018-01-09 01:30:32 -0800

    Update content
    Changed content for tax season. Replaced slider with Slick carousel. Updated Volunteer Portal to utilize more horizontal space.

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -3,30 +3,6 @@
     Import this at the bottom of index.html.
 */
 
-/* ResponsiveSlides.js v1.55 */
-$(function () {
-    $(".rslides").responsiveSlides();
-});
-
-$(".rslides").responsiveSlides({
-    auto: true,             // Boolean: Animate automatically, true or false
-    speed: 1000,            // Integer: Speed of the transition, in milliseconds
-    timeout: 5000,          // Integer: Time between slide transitions, in milliseconds
-    pager: false,           // Boolean: Show pager, true or false
-    nav: false,             // Boolean: Show navigation, true or false
-    random: false,          // Boolean: Randomize the order of the slides, true or false
-    pause: false,           // Boolean: Pause on hover, true or false
-    pauseControls: false,    // Boolean: Pause when hovering controls, true or false
-    prevText: "Previous",   // String: Text for the "previous" button
-    nextText: "Next",       // String: Text for the "next" button
-    maxwidth: "",           // Integer: Max-width of the slideshow, in pixels
-    navContainer: "",       // Selector: Where controls should be appended to, default is after the 'ul'
-    manualControls: "",     // Selector: Declare custom pager navigation
-    namespace: "rslides",   // String: Change the default namespace used
-    before: function () { },   // Function: Before callback
-    after: function () { }     // Function: After callback
-});
-
 /* Use Google Maps Javascript API to embed parking map */
 function initMap() {
     var ucla_center = { lat: 34.0775, lng: -118.4575 };
